cc78ead75e392bd3f4e55ff1e0a2d862aeeef712
LittleW <w-army@hotmail.co.th>
2025-10-07 16:10:35 +0700

    uwu

--- src/components/AFrame.tsx
+++ src/components/AFrame.tsx
@@ -1,8 +1,13 @@
 // @ts-nocheck
+
 export default function AFrame() {
     return (
-        
-        <a-scene embedded arjs>
+        <a-scene 
+            embedded 
+            arjs="sourceType: webcam; debugUIEnabled: false; trackingMethod: best; sourceWidth: 640; sourceHeight: 480; displayWidth: window.innerWidth; displayHeight: window.innerHeight;"
+            vr-mode-ui="enabled: false"
+            device-orientation-permission-ui="enabled: true"
+        >
             
             {/* <a-box position="0 0.5 0" 
                 rotation="90 0 0" 
@@ -11,15 +16,25 @@ export default function AFrame() {
             {/* <a-entity gltf-model="#pillow" position="0 0.25 0" scale="0.01 0.01 0.01"></a-entity> */}
 
             {/* GLB model */}
-            <a-entity 
-                gltf-model="url(./assets/neck_pillow.glb)" 
-                position="0 -0.1 0"
-                animation="property: rotation; to: 90 360 0; dur: 9000; loop: true"
-                scale="0.2ห 0.2 0.2">
-            </a-entity>
-            
+            <a-marker 
+              preset="hiro" 
+              smooth="true" 
+              smoothCount="10" 
+              smoothTolerance="0.01" 
+              smoothThreshold="5"
+            >
+                <a-entity 
+                    gltf-model={'url(./assets/neck_pillow.glb)'} 
+                    position="0 -0.1 0"
+                    animation="property: rotation; to: 90 360 0; dur: 9000; loop: true"
+                    scale="0.2 0.2 0.2">
+                </a-entity>
+                {/* <a-text value="Hello, World!"></a-text> */}
+            </a-marker>
+
             {/* Marker camera */}
-            <a-marker-camera preset="hiro"></a-marker-camera>
+            {/* <a-marker-camera preset="hiro"></a-marker-camera> */}
+            <a-entity camera></a-entity>
         </a-scene>
     )
 }
